fc38df9a569c25f80c339ec55eaa98ec05a4a3d6
Brian Michael Masiga <55559378+BrianMasiga@users.noreply.github.com>
2020-11-12 23:10:12 +0300

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -142,9 +142,16 @@
         this_form.find('.error-message').slideDown().html(msg);
       }
     }).fail( function(data){
-      this_form.find('.loading').slideUp();
+     if (msg == 'OK') {
+        this_form.find('.loading').slideUp();
         this_form.find('.sent-message').slideDown();
         this_form.find("input:not(input[type=submit]), textarea").val('');
+      } else {
+        this_form.find('.loading').slideUp();
+        if(!msg) {
+          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+        }
+        this_form.find('.error-message').slideDown().html(msg);
   }
 
 })(jQuery);
